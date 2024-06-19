17ee2ba4e4b212fb9c8b0a043339e5903a49b4fd
Edward Carney <carneyedwardj@gmail.com>
2024-06-19 00:57:03 -0400

    Refactors 2D maneuver

--- src/DubinsManeuver2d.cpp
+++ src/DubinsManeuver2d.cpp
@@ -1,80 +1,68 @@
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include <stdexcept>
+#include <vector>
 #include "DubinsManeuver2d.hpp"
+#include "Utility.hpp"
 
-void DubinsManeuver2d::_generateManeuver(double minLength, bool disableCCC)
-{
-    auto diff = qf() - qi();
-    double D = qf().distanceTo(qi());
+void DubinsManeuver2d::_generateManeuver(double minLength, bool disableCCC) {
+    namespace ranges = std::ranges;
 
-    double d = D / rhoMin();
+    const auto [x, y] = qf() - qi();
+    const double D = qf().distanceTo(qi());
 
-    double rotationAngle = Utility::mod2pi(atan2(diff.y, diff.x));
-    double a = Utility::mod2pi(qi().theta - rotationAngle);
-    double b = Utility::mod2pi(qf().theta - rotationAngle);
+    const double d = D / rhoMin();
 
-    double sa = sin(a);
-    double ca = cos(a);
-    double sb = sin(b);
-    double cb = cos(b);
+    const double rotationAngle = Utility::mod2pi(atan2(y, x));
+    const double a = Utility::mod2pi(qi().theta - rotationAngle);
+    const double b = Utility::mod2pi(qf().theta - rotationAngle);
 
-    DubinsParams params { a, b, d, sa, ca, sb, cb };
+    const double sa = std::sin(a);
+    const double ca = std::cos(a);
+    const double sb = std::sin(b);
+    const double cb = std::cos(b);
 
-    DubinsStruct pathRLR, pathLRL, pathC;
-    DubinsStruct pathLSL = _lsl(params);
-    DubinsStruct pathRSR = _rsr(params);
-    DubinsStruct pathLSR = _lsr(params);
-    DubinsStruct pathRSL = _rsl(params);
+    const DubinsParams params { a, b, d, sa, ca, sb, cb };
 
-    vector<DubinsStruct> paths;
-    if (disableCCC)
-    {
+    const DubinsStruct pathLSL = _lsl(params);
+    const DubinsStruct pathRSR = _rsr(params);
+    const DubinsStruct pathLSR = _lsr(params);
+    const DubinsStruct pathRSL = _rsl(params);
+
+    std::vector<DubinsStruct> paths;
+    if (disableCCC) {
         paths = { pathLSL, pathRSR, pathLSR, pathRSL };
     }
-    else
-    {
+    else {
+        DubinsStruct pathLRL;
+        DubinsStruct pathRLR;
         pathRLR = _rlr(params);
         pathLRL = _lrl(params);
         paths = { pathLSL, pathRSR, pathLSR, pathRSL, pathRLR, pathLRL };
     }
 
-    double rhoCompare = rhoMin() * 0.00001;
-    if (abs(d) < rhoCompare && abs(a) < rhoCompare && abs(b) < rhoCompare)
-    {
-        double dist2d = max(abs(qi().x - qf().x), abs(qi().y - qf().y));
-        if (dist2d < rhoCompare)
-        {
+    if (double rhoCompare = rhoMin() * 0.00001; std::abs(d) < rhoCompare && std::abs(a) < rhoCompare && std::abs(b) < rhoCompare) {
+        if (double dist2d = std::max(std::abs(qi().x - qf().x), std::abs(qi().y - qf().y)); dist2d < rhoCompare) {
+            DubinsStruct pathC;
             pathC = _c();
             paths = { pathC };
         }
     }
 
-    sort(paths.begin(), paths.end(),
-        [](const DubinsStruct& a, const DubinsStruct& b) -> bool
-        {
-            return a.length < b.length;
-        });
+    ranges::sort(paths, [](const auto &x1, const auto &x2){ return x1.length < x2.length; });
 
-    if (minLength < 0)
-    {
+    if (minLength < 0) {
         _maneuver = paths[0];
     }
-    else
-    {
-        for (auto p : paths)
-        {
-            if (p.length >= minLength)
-            {
-                _maneuver = p;
-                break;
-            }
-        }
+    else if (const auto first = ranges::find_if(paths, [minLength](const auto &p){ return p.length >= minLength; }); first != paths.end()) {
+        _maneuver = *first;
     }
 }
 
-State2d DubinsManeuver2d::_getPositionInSegment(double offset, State2d qi, char caseType) const
-{
+State2d DubinsManeuver2d::_getPositionInSegment(const double offset, const State2d& qi, const char caseType) {
     State2d q = { 0, 0, 0 };
-    switch (caseType)
-    {
+    switch (caseType) {
         case 'L':
             q.x = qi.x + sin(qi.theta + offset) - sin(qi.theta);
             q.y = qi.y - cos(qi.theta + offset) + cos(qi.theta);
@@ -90,173 +78,131 @@ State2d DubinsManeuver2d::_getPositionInSegment(double offset, State2d qi, char 
             q.y = qi.y + sin(qi.theta) * offset;
             q.theta = qi.theta;
             break;
+        default:
+            throw std::runtime_error("Unknown segment case type");
     }
     return q;
 }
 
-DubinsStruct DubinsManeuver2d::_lsl(const DubinsParams& params) const
-{
-    double aux, t, p, q, length;
-    aux = atan2(params.cb - params.ca, params.d + params.sa - params.sb);
-    t = Utility::mod2pi(-params.a + aux);
-    p = sqrt(2 + params.d*params.d - 2*cos(params.a - params.b) + 2*params.d*(params.sa - params.sb));
-    q = Utility::mod2pi(params.b - aux);
-    length = (t + p + q) * rhoMin();
-    string caseType = "LSL";
+DubinsStruct DubinsManeuver2d::_lsl(const DubinsParams& params) const {
+    const double aux = atan2(params.cb - params.ca, params.d + params.sa - params.sb);
+    const double t = Utility::mod2pi(-params.a + aux);
+    const double p = sqrt(2 + params.d * params.d - 2 * cos(params.a - params.b) + 2 * params.d * (params.sa - params.sb));
+    const double q = Utility::mod2pi(params.b - aux);
+    const double length = (t + p + q) * rhoMin();
+    const std::string caseType = "LSL";
     return DubinsStruct {t, p, q, length, caseType};
 }
 
-DubinsStruct DubinsManeuver2d::_rsr(const DubinsParams& params) const
-{
-    double aux, t, p, q, length;
-    aux = atan2(params.ca - params.cb, params.d - params.sa + params.sb);
-    t = Utility::mod2pi(params.a - aux);
-    p = sqrt(2 + params.d*params.d - 2*cos(params.a - params.b) + 2*params.d*(params.sb - params.sa));
-    q = Utility::mod2pi(Utility::mod2pi(-params.b) + aux);
-    length = (t + p + q) * rhoMin();
-    string caseType = "RSR";
+DubinsStruct DubinsManeuver2d::_rsr(const DubinsParams& params) const {
+    const double aux = atan2(params.ca - params.cb, params.d - params.sa + params.sb);
+    const double t = Utility::mod2pi(params.a - aux);
+    const double p = sqrt(2 + params.d * params.d - 2 * cos(params.a - params.b) + 2 * params.d * (params.sb - params.sa));
+    const double q = Utility::mod2pi(Utility::mod2pi(-params.b) + aux);
+    const double length = (t + p + q) * rhoMin();
+    const std::string caseType = "RSR";
     return DubinsStruct {t, p, q, length, caseType};
 }
 
-DubinsStruct DubinsManeuver2d::_lsr(const DubinsParams& params) const
-{
-    double aux1, aux2, t, p, q, length;
-    aux1 = -2 + params.d*params.d + 2*cos(params.a - params.b) + 2*params.d*(params.sa + params.sb);
-    if (aux1 > 0)
-    {
+DubinsStruct DubinsManeuver2d::_lsr(const DubinsParams& params) const {
+    double t, p, q;
+    const double aux1 = -2 + params.d * params.d + 2 * cos(params.a - params.b) + 2 * params.d * (params.sa + params.sb);
+    if (aux1 > 0) {
         p = sqrt(aux1);
-        aux2 = atan2(-params.ca-params.cb, params.d+params.sa+params.sb) - atan(-2/p);
+        const double aux2 = atan2(-params.ca - params.cb, params.d + params.sa + params.sb) - atan(-2 / p);
         t = Utility::mod2pi(-params.a + aux2);
         q = Utility::mod2pi(-Utility::mod2pi(params.b) + aux2);
     }
-    else
-    {
-        t = INFINITY;
-        p = INFINITY;
-        q = INFINITY;
+    else {
+        t = std::numeric_limits<double>::infinity();
+        p = std::numeric_limits<double>::infinity();
+        q = std::numeric_limits<double>::infinity();
     }
-    length = (t+p+q) * rhoMin();
-    string caseType = "LSR";
+    const double length = (t + p + q) * rhoMin();
+    const std::string caseType = "LSR";
     return DubinsStruct {t, p, q, length, caseType};
 }
 
-DubinsStruct DubinsManeuver2d::_rsl(const DubinsParams& params) const
-{
-    double aux1, aux2, t, p, q, length;
-    aux1 = params.d*params.d - 2 + 2*cos(params.a-params.b) - 2*params.d*(params.sa+params.sb);
-    if (aux1 > 0)
-    {
+DubinsStruct DubinsManeuver2d::_rsl(const DubinsParams& params) const {
+    double t, p, q;
+    const double aux1 = params.d * params.d - 2 + 2 * cos(params.a - params.b) - 2 * params.d * (params.sa + params.sb);
+    if (aux1 > 0) {
         p = sqrt(aux1);
-        aux2 = atan2(params.ca+params.cb, params.d-params.sa-params.sb) - atan(2/p);
+        const double aux2 = atan2(params.ca + params.cb, params.d - params.sa - params.sb) - atan(2 / p);
         t = Utility::mod2pi(params.a - aux2);
         q = Utility::mod2pi(Utility::mod2pi(params.b) - aux2);
     }
-    else
-    {
-        t = INFINITY;
-        p = INFINITY;
-        q = INFINITY;
+    else {
+        t = std::numeric_limits<double>::infinity();
+        p = std::numeric_limits<double>::infinity();
+        q = std::numeric_limits<double>::infinity();
     }
-    length = (t+p+q) * rhoMin();
-    string caseType = "RSL";
+    const double length = (t + p + q) * rhoMin();
+    const std::string caseType = "RSL";
     return DubinsStruct {t, p, q, length, caseType};
 }
 
-DubinsStruct DubinsManeuver2d::_rlr(const DubinsParams& params) const
-{
-    double aux, t, p, q, length;
-    string caseType;
-    aux = (6 - params.d*params.d + 2*cos(params.a-params.b) + 2*params.d*(params.sa-params.sb))/8;
-    if (abs(aux) <= 1)
-    {
+DubinsStruct DubinsManeuver2d::_rlr(const DubinsParams& params) const {
+    double t, p, q;
+    const double aux = (6 - params.d * params.d + 2 * cos(params.a - params.b) + 2 * params.d * (params.sa - params.sb)) / 8;
+    if (std::abs(aux) <= 1) {
         p = Utility::mod2pi(-acos(aux));
         t = Utility::mod2pi(params.a - atan2(params.ca-params.cb, params.d-params.sa+params.sb) + p/2);
         q = Utility::mod2pi(params.a - params.b - t + p);
     }
-    else
-    {
-        t = INFINITY;
-        p = INFINITY;
-        q = INFINITY;
+    else {
+        t = std::numeric_limits<double>::infinity();
+        p = std::numeric_limits<double>::infinity();
+        q = std::numeric_limits<double>::infinity();
     }
-    length = (t+p+q) * rhoMin();
-    caseType = "RLR";
+    const double length = (t + p + q) * rhoMin();
+    const std::string caseType = "RLR";
     return DubinsStruct {t, p, q, length, caseType};
 }
 
-DubinsStruct DubinsManeuver2d::_lrl(const DubinsParams& params) const
-{
-    double aux, t, p, q, length;
-    aux = (6 - params.d*params.d + 2*cos(params.a-params.b) + 2*params.d*(-params.sa+params.sb))/8;;
-    if (abs(aux) <= 1)
-    {
+DubinsStruct DubinsManeuver2d::_lrl(const DubinsParams& params) const {
+    double t, p, q;
+    const double aux = (6 - params.d * params.d + 2 * cos(params.a - params.b) + 2 * params.d * (-params.sa + params.sb)) / 8;;
+    if (std::abs(aux) <= 1) {
         p = Utility::mod2pi(-acos(aux));
         t = Utility::mod2pi(-params.a + atan2(-params.ca+params.cb, params.d+params.sa-params.sb) + p/2);
         q = Utility::mod2pi(params.b - params.a - t + p);
     }
-    else
-    {
-        t = INFINITY;
-        p = INFINITY;
-        q = INFINITY;
+    else {
+        t = std::numeric_limits<double>::infinity();
+        p = std::numeric_limits<double>::infinity();
+        q = std::numeric_limits<double>::infinity();
     }
-    length = (t+p+q) * rhoMin();
-    string caseType = "LRL";
+    const double length = (t + p + q) * rhoMin();
+    const std::string caseType = "LRL";
     return DubinsStruct {t, p, q, length, caseType};
 }
 
-DubinsStruct DubinsManeuver2d::_c() const
-{
-    double t, p , q, length;
-    t = 0;
-    p = 2*M_PI;
-    q = 0;
-    length = (t+p+q) * rhoMin();
-    string caseType = "RRR";
+DubinsStruct DubinsManeuver2d::_c() const {
+    const double t = 0;
+    const double p = 2 * std::numbers::pi;
+    const double q = 0;
+    const double length = (t + p + q) * rhoMin();
+    const std::string caseType = "RRR";
     return DubinsStruct {t, p, q, length, caseType};
 }
 
-double DubinsManeuver2d::rhoMin() const { return _rhoMin; }
-
-const State2d& DubinsManeuver2d::qi() const { return _qi; }
-
-const State2d& DubinsManeuver2d::qf() const { return _qf; }
-
-const DubinsStruct& DubinsManeuver2d::maneuver() const { return _maneuver; }
-
-void DubinsManeuver2d::setManeuver(DubinsStruct maneuver) { _maneuver = maneuver; }
-
-DubinsManeuver2d::DubinsManeuver2d() { }
-
-DubinsManeuver2d::DubinsManeuver2d(const State2d& qi, const State2d& qf, double rhoMin, double minLength, bool disableCCC)
-{
-    _qi = qi;
-    _qf = qf;
-    _rhoMin = rhoMin;
-    _maneuver = DubinsStruct {0, 0, 0, INFINITY, ""};
-    _generateManeuver(minLength, disableCCC);
-}
-
-State2d DubinsManeuver2d::getCoordinatesAt(double offset) const
-{
-    double noffset = offset / rhoMin();
-    State2d qir = { 0, 0, qi().theta };
+State2d DubinsManeuver2d::getCoordinatesAt(const double offset) const {
+    const double noffset = offset / rhoMin();
+    const State2d qir = { 0, 0, qi().theta };
     State2d q, q1, q2;
 
-    double l1 = maneuver().t;
-    double l2 = maneuver().p;
+    const double l1 = maneuver().t;
+    const double l2 = maneuver().p;
 
-    if (noffset < l1)
-    {
+    if (noffset < l1) {
         q = _getPositionInSegment(noffset, qir, maneuver().caseType.at(0));
     }
-    else if(noffset < (l1 + l2))
-    {
+    else if(noffset < (l1 + l2)) {
         q1 = _getPositionInSegment(l1, qir, maneuver().caseType.at(0));
         q = _getPositionInSegment(noffset - l1, q1, maneuver().caseType.at(1));
     }
-    else
-    {
+    else {
         q1 = _getPositionInSegment(l1, qir, maneuver().caseType.at(0));
         q2 = _getPositionInSegment(l2, q1, maneuver().caseType.at(1));
         q = _getPositionInSegment(noffset - l1 - l2, q2, maneuver().caseType.at(2));
@@ -269,13 +215,11 @@ State2d DubinsManeuver2d::getCoordinatesAt(double offset) const
     return q;
 }
 
-vector<State2d> DubinsManeuver2d::getSamplingPoints(double res) const
-{
-    int numPoints = (int)floor(maneuver().length / res) + 1;
-    vector<State2d> points(numPoints);
+std::vector<State2d> DubinsManeuver2d::getSamplingPoints(const double res) const {
+    const int numPoints = static_cast<int>(std::floor(maneuver().length / res)) + 1;
+    std::vector<State2d> points(numPoints);
 
-    for (int i = 0; i < numPoints; i++)
-    {
+    for (int i = 0; i < numPoints; i++) {
         points.push_back(getCoordinatesAt(i * res));
     }
 
--- src/DubinsManeuver2d.hpp
+++ src/DubinsManeuver2d.hpp
@@ -1,52 +1,47 @@
-#include <math.h>
-#include <string>
-#include <vector>
-#include "Geometry.hpp"
-#include "Utility.hpp"
-
-using namespace std;
-
 #ifndef DUBINS_MANEUVER_2D
 #define DUBINS_MANEUVER_2D
 
-struct DubinsStruct
-{
+#include <string>
+#include <vector>
+#include "Geometry.hpp"
+
+struct DubinsStruct {
     double t, p, q, length;
-    string caseType;
+    std::string caseType;
 };
 
-struct DubinsParams
-{
+struct DubinsParams {
     double a, b, d, sa, ca, sb, cb;
 };
 
-class DubinsManeuver2d
-{
-    double _rhoMin;
+class DubinsManeuver2d {
     State2d _qi, _qf;
+    double _rhoMin;
     DubinsStruct _maneuver;
 
     void _generateManeuver(double minLength, bool disableCCC);
-    State2d _getPositionInSegment(double offset, State2d qi, char caseType) const;
-    DubinsStruct _lsl(const DubinsParams& params) const;
-    DubinsStruct _rsr(const DubinsParams& params) const;
-    DubinsStruct _lsr(const DubinsParams& params) const;
-    DubinsStruct _rsl(const DubinsParams& params) const;
-    DubinsStruct _rlr(const DubinsParams& params) const;
-    DubinsStruct _lrl(const DubinsParams& params) const;
-    DubinsStruct _c() const;
+    [[nodiscard]] static State2d _getPositionInSegment(double offset, const State2d& qi, char caseType);
+    [[nodiscard]] DubinsStruct _lsl(const DubinsParams& params) const;
+    [[nodiscard]] DubinsStruct _rsr(const DubinsParams& params) const;
+    [[nodiscard]] DubinsStruct _lsr(const DubinsParams& params) const;
+    [[nodiscard]] DubinsStruct _rsl(const DubinsParams& params) const;
+    [[nodiscard]] DubinsStruct _rlr(const DubinsParams& params) const;
+    [[nodiscard]] DubinsStruct _lrl(const DubinsParams& params) const;
+    [[nodiscard]] DubinsStruct _c() const;
 
     public:
-        double rhoMin() const;
-        const State2d& qi() const;
-        const State2d& qf() const;
-        const DubinsStruct& maneuver() const;
-        void setManeuver(DubinsStruct maneuver);
+        DubinsManeuver2d() = default;
+        DubinsManeuver2d(const State2d& qi, const State2d& qf, const double rhoMin = 1, double minLength = -1, bool disableCCC = false)
+            : _qi{qi}, _qf{qf}, _rhoMin{rhoMin}, _maneuver{0,0,0,std::numeric_limits<double>::infinity(), ""}
+            { _generateManeuver(minLength, disableCCC); }
+        [[nodiscard]] double rhoMin() const { return _rhoMin; }
+        [[nodiscard]] const State2d& qi() const { return _qi; }
+        [[nodiscard]] const State2d& qf() const { return _qf; };
+        [[nodiscard]] const DubinsStruct& maneuver() const { return _maneuver; }
+        void setManeuver(DubinsStruct maneuver) { _maneuver = _maneuver; }
 
-        State2d getCoordinatesAt(double offset) const;
-        vector<State2d> getSamplingPoints(double res = 0.1) const;
-        DubinsManeuver2d();
-        DubinsManeuver2d(const State2d& qi, const State2d& qf, double rhoMin = 1, double minLength = -1, bool disableCCC = false);
+        [[nodiscard]] State2d getCoordinatesAt(double offset) const;
+        [[nodiscard]] std::vector<State2d> getSamplingPoints(double res = 0.1) const;
 };
 
 #endif //DUBINS_MANEUVER_2D
