1edcc2a8ce90c158ab8bc76ff6b7fbd4c653f741
Edward Carney <carneyedwardj@gmail.com>
2024-06-19 00:05:03 -0400

    Refactors geometry

--- src/Geometry.hpp
+++ src/Geometry.hpp
@@ -1,30 +1,26 @@
-#include <math.h>
-
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
-struct Point2d
-{
+#include <cmath>
+
+struct Point2d {
     double x, y;
 
     Point2d operator+(const Point2d& p) const { return Point2d { x + p.x, y + p.y }; }
     Point2d operator-(const Point2d& p) const { return Point2d { x - p.x, y - p.y }; }
-    double distanceTo(const Point2d& p) const { return hypot(x - p.x, y - p.y); }
+    [[nodiscard]] double distanceTo(const Point2d& p) const { return std::hypot(x - p.x, y - p.y); }
 };
 
-struct Point3d
-{
+struct Point3d {
     double x, y, z;
 }; 
 
-struct State2d : public Point2d
-{
+struct State2d : Point2d {
     double theta;
 };
 
-struct State3d : public Point3d
-{
+struct State3d : Point3d {
     double theta, gamma;
 };
 
-#endif //
+#endif // GEOMETRY_H
